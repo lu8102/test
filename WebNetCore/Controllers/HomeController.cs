8c0c19e3570a75d29071e21a509998eccf234bb8
lu8102@gmail.com <lu8102@gmail.com>
2020-04-07 20:47:51 +0900

    initial commit

---
+++ WebNetCore/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebNetCore.Models;
+
+namespace WebNetCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ILogger<HomeController> _logger;
+        private readonly IServiceProvider _serviceProvider;
+
+        public HomeController(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public IActionResult Index()
+        {
+            _logger = (ILogger<HomeController>) _serviceProvider.GetService(typeof(ILogger<HomeController>));
+            _logger.LogInformation("テスト");
+            return View();
+        }
+    }
+}
